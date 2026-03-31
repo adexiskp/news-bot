0c889eedc3f25f92122e25a4dfd237bceddde6d3
adexiskp <opyd.pawel8@gmail.com>
2026-03-31 02:12:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
+import os
 import requests
 import schedule
 import time
-from datetime import datetime
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1488317285318918164/njcqeAgHoa-Gkce2Sit4FY1F_0CFQkH3-BSAPRrFA2ZrTGuFzhaAgUiKgClLOHJbUCtZ"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 def send_news():
-    today = datetime.now().strftime("%d.%m.%Y")
-
     embed = {
-        "title": "📢 Kalendarz ekonomiczny",
-        "description": (
-            f"🇺🇸 **USD - Liczba ofert pracy JOLTS**\n"
-            f"📅 **{today}**\n"
-            f"🕒 **15:00**\n"
-            f"🔴 **Wysoki wpływ na rynek**\n\n"
-            f"📈 **Prognoza:** 6.89M\n"
-            f"📊 **Poprzedni odczyt:** 6.95M"
-        ),
-        "color": 16711680,
-        "footer": {
-            "text": "Automatyczne wiadomości • Bot ekonomiczny"
-        }
+        "title": "📅 Kalendarz ekonomiczny",
+        "description": "🇺🇸 USD - JOLTS Job Openings\n🔴 WYSOKI WPŁYW",
+        "color": 16711680
     }
 
     requests.post(WEBHOOK_URL, json={"embeds": [embed]})
-    print("✅ Wysłano nową wiadomość")
+    print("Wysłano")
 
 schedule.every().day.at("01:00").do(send_news)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
